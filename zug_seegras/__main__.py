aad2c231a2d03809e00865280f3d7c00efd0d709
Nikolai Lorenz <nikolai.lorenz@birdsonmars.com>
2024-11-27 14:02:30 +0000

    Implement new pre-commit hooks and run on all files

--- zug_seegras/__main__.py
+++ zug_seegras/__main__.py
@@ -1,10 +1,12 @@
-from zug_seegras import getLogger
-from zug_seegras import BaseClass
+from zug_seegras import BaseClass, getLogger
 
 log = getLogger(__name__)
+
+
 def main() -> str:
     log.info("Starting up")
     base = BaseClass()
     return base.hello_world()
+
 
 print(main())
