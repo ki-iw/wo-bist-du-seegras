4872d480cd0105ae39e81876a61e8d866f801956
Nikolai Lorenz <nikolai.lorenz@birdsonmars.com>
2024-11-28 11:42:24 +0000

    Implement binary resnet18 tests

---
+++ tests/test_binary_resnet18.py
@@ -0,0 +1,23 @@
+import pytest
+import torch
+import torch.nn as nn
+
+from zug_seegras.core.classification_models import BinaryResNet18
+
+
+@pytest.fixture
+def resnet18_fixture():
+    return BinaryResNet18(False, 2)
+
+
+def test_resnet18_initialization(resnet18_fixture):
+    model = resnet18_fixture.get_model()
+    assert isinstance(model, nn.Module)
+
+
+def test_get_resnet18_forward_pass(resnet18_fixture):
+    input_tensor = torch.randn(1, 3, 512, 512)
+    want = (1, 2)
+    model = resnet18_fixture.get_model()
+    got = model(input_tensor).shape
+    assert got == want
