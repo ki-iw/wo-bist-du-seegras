d159725533d6df35839f7034db1fc1f9f6125c27
Nikolai Lorenz <nikolai.lorenz@birdsonmars.com>
2024-12-04 09:21:10 +0000

    Update Evaluator to be able to pass a model to run_evaluation

--- zug_seegras/core/evaluator.py
+++ zug_seegras/core/evaluator.py
@@ -19,20 +19,22 @@ class Evaluator:
         checkpoint_path: Optional[str] = None,  # noqa: UP007
         n_classes: int = 2,
         transforms=None,
+        device: Optional[torch.device] = None,  # noqa: UP007
     ) -> None:
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.device = device if device else torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.transforms = transforms
 
-        model_factory = ModelFactory(self.device)
+        model_factory = None
 
         if model is not None:
             self.model = model
         elif model_name is not None:
+            model_factory = ModelFactory(self.device)
             self.model = model_factory.create_model(model_name=model_name, n_classes=n_classes)
         else:
             raise ValueError("Either a model or a model_name must be provided.")  # noqa: TRY003
 
-        if checkpoint_path is not None:
+        if model_factory and checkpoint_path:
             model_factory.load_checkpoint(self.model, checkpoint_path)
 
         self.model.eval()
@@ -63,6 +65,7 @@ class Evaluator:
 
     def run_evaluation(
         self,
+        model: Optional[nn.Module] = None,  # noqa: UP007
         dataloader: Optional[DataLoader] = None,  # noqa: UP007
         dataset: Optional[Dataset] = None,  # noqa: UP007
         batch_size: int = 1,
@@ -72,6 +75,9 @@ class Evaluator:
             if not dataset:
                 raise ValueError("Either 'dataloader' or 'dataset' must be provided!")  # noqa: TRY003
             dataloader = self._prepare_dataloader(dataset, batch_size, shuffle)
+
+        if model is None:
+            model = self.model
 
         all_labels = []
         all_predictions = []
