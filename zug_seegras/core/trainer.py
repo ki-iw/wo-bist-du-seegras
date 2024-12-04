4481f9b2345e1f12a229f2991fb103e34d367112
Nikolai Lorenz <nikolai.lorenz@birdsonmars.com>
2024-12-04 10:28:12 +0000

    Implement tqdm progress bar and BCE Loss

--- zug_seegras/core/trainer.py
+++ zug_seegras/core/trainer.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 import torch.optim as optim
 import yaml
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 from zug_seegras.core.evaluator import Evaluator
 from zug_seegras.core.model_factory import ModelFactory
@@ -33,7 +34,7 @@ class Trainer:
         self.model = self.initialize_model(checkpoint_path)
         self.optimizer = self.initialize_optimizer()
 
-        self.evaluator = Evaluator(transforms=self.train_loader.dataset.transform, device=self.device)
+        self.evaluator = Evaluator(device=self.device)
 
     @staticmethod
     def load_config(config_path: str):
@@ -56,6 +57,8 @@ class Trainer:
         loss_name = self.config.get("training").get("loss_function", "CrossEntropyLoss")
         if loss_name == "CrossEntropyLoss":
             return nn.CrossEntropyLoss()
+        elif loss_name == "BinaryCrossEntropy":
+            return nn.BCELoss()
         raise NotImplementedError(f"Loss function '{loss_name}' is not implemented.")
 
     def initialize_optimizer(self):
@@ -75,16 +78,17 @@ class Trainer:
         os.makedirs(model_checkpoint_dir, exist_ok=True)
 
         self.model.to(self.device)
-        for epoch in range(self.current_epoch, num_epochs):
+        for epoch in tqdm(range(self.current_epoch, num_epochs), desc="Epochs", total=num_epochs - self.current_epoch):
             self.model.train()
             running_loss = 0.0
 
-            for inputs, labels in self.train_loader:
+            for inputs, labels in tqdm(self.train_loader, desc=f"Epoch {epoch + 1}/{num_epochs}", leave=False):
                 inputs, labels = inputs.to(self.device), labels.to(self.device)
 
                 self.optimizer.zero_grad()
-
+                labels = labels.unsqueeze(1).float()
                 outputs = self.model(inputs)
+
                 loss = self.criterion(outputs, labels)
 
                 loss.backward()
@@ -92,12 +96,12 @@ class Trainer:
 
                 running_loss += loss.item()
 
-            log.info(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {running_loss / len(self.train_loader):.4f}")
+            tqdm.write(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {running_loss / len(self.train_loader):.4f}")
 
             if (epoch + 1) % 5 == 0:
                 accuracy, f1_score = self.evaluator.run_evaluation(model=self.model, dataloader=self.test_loader)
-                log.info(f"Epoch [{epoch + 1}], Accuracy: {accuracy:.4f}, F1 Score: {f1_score:.4f}")
+                tqdm.write(f"Epoch [{epoch + 1}], Accuracy: {accuracy:.4f}, F1 Score: {f1_score:.4f}")
 
                 checkpoint_path = os.path.join(model_checkpoint_dir, f"{model_name}_{epoch + 1}.pth")
                 self.model_factory.save_checkpoint(self.model, self.optimizer, checkpoint_path, epoch + 1)
-                log.debug(f"Model checkpoint saved at {checkpoint_path}")
+                tqdm.write(f"Model checkpoint saved at {checkpoint_path}")
