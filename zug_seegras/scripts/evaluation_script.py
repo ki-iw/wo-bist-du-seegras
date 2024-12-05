db37ffdbbf83c763d98816a67bc2577ccc392095
Nikolai Lorenz <nikolai.lorenz@birdsonmars.com>
2024-12-05 15:08:24 +0000

    Refactor evaluation_script to match create_dataloaders

--- zug_seegras/scripts/evaluation_script.py
+++ zug_seegras/scripts/evaluation_script.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import torch
 from torchvision.transforms import Compose, Normalize, Resize, ToTensor
 
@@ -11,11 +9,8 @@ from zug_seegras.logger import getLogger
 log = getLogger(__name__)
 
 
-def main():
-    data_path = Path("data")
-    video_file = data_path / "input_video" / "trimmed_testvideo.mov"
-    label_json_path = data_path / "input_label" / "default.json"
-    output_frames_dir = data_path / "output"
+def main(model_name: str):
+    data_path = "data/Seegras_v1"
 
     transforms = Compose(
         [Resize((512, 512)), ToTensor(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]
@@ -23,20 +18,19 @@ def main():
 
     _, test_loader = create_dataloaders(
         dataset_class=SeegrasDataset,
-        video_file=str(video_file),
-        label_json_path=str(label_json_path),
-        output_frames_dir=str(output_frames_dir),
+        dataset_dir=data_path,
         transform=transforms,
         batch_size=4,
         train_test_ratio=0.8,
         shuffle=True,
     )
 
-    evaluator = Evaluator(model_name="seaclips", device="cuda" if torch.cuda.is_available() else "cpu")
+    evaluator = Evaluator(model_name=model_name, device="cuda" if torch.cuda.is_available() else "cpu")
     accuracy, f1_score = evaluator.run_evaluation(dataloader=test_loader)
 
     log.info(f"Accuracy: {accuracy:.4f}, F1 Score: {f1_score:.4f}")
 
 
 if __name__ == "__main__":
-    main()
+    model_name = "grounding_dino"
+    main(model_name)
